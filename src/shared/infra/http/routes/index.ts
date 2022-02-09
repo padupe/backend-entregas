33c5e5b4f190b4302e4e86e529b21d02408cb64e
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-09 19:57:57 -0300

    feat(createDeliveryman): call route "deliveryman.routes.ts". Dir: src/shared/infra/http/routes.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { clientsRoutes } from "./clients.routes";
 import { authenticateRoutes } from "./authenticate.routes";
+import { deliverymansRoutes } from "./deliveryman.routes";
 
 const router = Router();
 
 router.use("/clients", clientsRoutes);
 router.use("/authenticate", authenticateRoutes);
+router.use("/deliveryman", deliverymansRoutes);
 
 export { router };

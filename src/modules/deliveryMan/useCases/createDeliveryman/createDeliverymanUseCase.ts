3587d7b799bdb6dd582d7c714bb3cf164e7a6aef
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-09 19:55:10 -0300

    feat(createDeliverman): added to the Project file "createDeliverymanUseCase.ts".

---
+++ src/modules/deliveryMan/useCases/createDeliveryman/createDeliverymanUseCase.ts
@@ -0,0 +1,52 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from "bcrypt";
+import { AppError } from "../../../../shared/errors/appError";
+
+interface ICreateDeliveryman {
+    email: string;
+    username: string;
+    password: string;
+};
+
+interface IResponseCreateDeliveryman {
+    message: string;
+    deliveryman: Object;
+}
+
+export class CreateDeliverymanUseCase {
+    async execute({ email, username, password }: ICreateDeliveryman): Promise<IResponseCreateDeliveryman> {
+        
+        const usernameExists = await prisma.deliverymans.findFirst({
+            where: {
+                username: {
+                    mode: "insensitive"
+                }
+            }
+        });
+
+        const emailExists = await prisma.deliverymans.findFirst({
+            where: {
+                email
+            }
+        })
+        
+        if (usernameExists || emailExists ) {
+            throw new AppError('Deliveryman already exists!');
+        };
+
+        const hashPass = await hash(password, 10);
+
+        const newDeliveryman = await prisma.deliverymans.create({
+            data: {
+                email,
+                username,
+                password: hashPass
+            }
+        });
+
+        return {
+            message: "Deliveryman registered successfully",
+            deliveryman: newDeliveryman
+        };
+    };
+};

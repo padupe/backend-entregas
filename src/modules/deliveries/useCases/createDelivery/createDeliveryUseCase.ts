eceaadaa33df68086384ae17e99ef0cebefc2ad6
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-10 09:09:10 -0300

    feat(deliveries): create a delivery. Create "createDeliveryUseCase.ts" on src/modules/deliveries/useCases/createDelivery.

---
+++ src/modules/deliveries/useCases/createDelivery/createDeliveryUseCase.ts
@@ -0,0 +1,21 @@
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateDelivery {
+    item_name: string;
+    id_client: string;
+}
+
+export class CreateDeliveryUseCase {
+
+    async execute({ item_name, id_client }: ICreateDelivery) {
+
+        const newDelivery = await prisma.deliveries.create({
+            data: {
+                item_name,
+                id_client
+            }
+        });
+
+        return newDelivery
+    };
+};

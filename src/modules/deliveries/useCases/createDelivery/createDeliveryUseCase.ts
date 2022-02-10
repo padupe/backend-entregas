0c28e309d2511a261346d885335ab583f63861f9
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-10 18:15:48 -0300

    refactor(deliveries): configuration of return createDelivery.

--- src/modules/deliveries/useCases/createDelivery/createDeliveryUseCase.ts
+++ src/modules/deliveries/useCases/createDelivery/createDeliveryUseCase.ts
@@ -3,19 +3,41 @@ import { prisma } from "../../../../database/prismaClient";
 interface ICreateDelivery {
     item_name: string;
     id_client: string;
+};
+
+interface IResponseDelivery {
+    id: string;
+    item: string;
+    client: Object;
 }
 
 export class CreateDeliveryUseCase {
 
-    async execute({ item_name, id_client }: ICreateDelivery) {
+    async execute({ item_name, id_client }: ICreateDelivery): Promise<IResponseDelivery> {
 
         const newDelivery = await prisma.deliveries.create({
             data: {
                 item_name,
                 id_client
+            },
+            include: {
+                client: true
             }
         });
 
-        return newDelivery
+        const clientDelivery = await prisma.clients.findUnique({
+            where: {
+                id: id_client
+            }
+        });
+
+        return {
+            id: newDelivery.id,
+            item: newDelivery.item_name,
+            client: {
+                username: clientDelivery?.username,
+                id: newDelivery.id_client
+            }
+        }
     };
 };

5b11004dc7aa7bcdce74e9483216daa253c33b15
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-10 08:45:02 -0300

    feat(routes): deliveryman authenticate. Update "authenticate.routes.ts" on src/shared/infra/http/routes.

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "../../../../modules/account/useCases/authClient/authenticateClientController";
+import { AuthenticateDeliverymanController } from "../../../../modules/account/useCases/authDeliveryman/authenticateDeliverymanController";
 
 
 const authenticateRoutes = Router();
 
 const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
 authenticateRoutes.post("/clients", authenticateClientController.handle);
+authenticateRoutes.post("/deliverymans", authenticateDeliverymanController.handle);
 
 export { authenticateRoutes };

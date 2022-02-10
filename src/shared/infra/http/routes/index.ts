c7e6a840316515ee807d96e168b9a902e0f3c64d
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-10 09:11:35 -0300

    feat(routes): create a delivery. Update "index.ts" on src/shared/infra/http/routes.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { clientsRoutes } from "./clients.routes";
 import { authenticateRoutes } from "./authenticate.routes";
 import { deliverymansRoutes } from "./deliveryman.routes";
+import { deliveriesRoutes } from "./deliveries.routes";
 
 const router = Router();
 
 router.use("/clients", clientsRoutes);
 router.use("/authenticate", authenticateRoutes);
 router.use("/deliveryman", deliverymansRoutes);
+router.use("/deliveries", deliveriesRoutes);
 
 export { router };

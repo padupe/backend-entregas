b205d32cb081f7da6a72497256495087516f32d6
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-11 20:43:18 -0300

    feat(deliveries): create "updateEndDate".

---
+++ src/modules/deliveries/useCases/updateEndDate/updateEndDateUsecase.ts
@@ -0,0 +1,71 @@
+import { prisma } from "../../../../database/prismaClient";
+import { AppError } from "../../../../shared/errors/appError";
+
+interface IUpdateEndDate {
+    id_delivery: string;
+    id_deliveryman: string;
+}
+
+interface IResponseUpdateEndDate {
+    id_delivery: string;
+    client: string;
+    item: string;
+    created_at: Date;
+    end_date?: Date;
+    deliveryman?: string;
+}
+
+export class UpdateEndDateUseCase {
+
+    async execute({ id_delivery, id_deliveryman }:IUpdateEndDate): Promise<IResponseUpdateEndDate> {
+
+        const findDelivery = await prisma.deliveries.findUnique({
+            where: {
+                id: id_delivery
+            }
+        })
+
+        if(!findDelivery) {
+            throw new AppError("Delivery not found!")
+        }
+
+        if(findDelivery.id_deliveryman != id_deliveryman) {
+            throw new AppError("Deliveryman is not responsible for delivery", 406)
+        }
+
+        const update = await prisma.deliveries.update({
+            where: {
+                id: id_delivery
+            },
+            data: {
+                end_date: new Date()
+            },
+            select: {
+                id: true,
+                item_name: true,
+                created_at: true,
+                end_date: true,
+                client: {
+                    select: {
+                        username: true,
+                    }
+                },
+                deliveryman: {
+                    select: {
+                        username: true
+                    }
+                }
+            },
+        });
+
+        return {
+            id_delivery,
+            client: update.client.username,
+            item: update.item_name,
+            created_at: update.created_at,
+            //@ts-ignore
+            end_date: update.end_date,            
+            deliveryman: update.deliveryman?.username
+        };
+    }
+}
--- src/shared/infra/http/routes/deliveries.routes.ts
+++ src/shared/infra/http/routes/deliveries.routes.ts
@@ -4,15 +4,18 @@ import { ensureAuthenticateDeliveryman } from "../../../../middlewares/ensureAut
 import { CreateDeliveryController } from "../../../../modules/deliveries/useCases/createDelivery/createDeliveryController";
 import { FindAllDeliveriesAvailableController } from "../../../../modules/deliveries/useCases/findAllDeliveryAvailable/findAllDeliveriesAvailableController";
 import { UpdateDeliveryManController } from "../../../../modules/deliveries/useCases/updateDeliveryman/updateDeliverymanController";
+import { UpdateEndDateController } from "../../../../modules/deliveries/useCases/updateEndDate/updateEndDateController";
 
 const deliveriesRoutes = Router();
 
 const createDeliverController = new CreateDeliveryController();
 const findAllDeliveriesAvailableController = new FindAllDeliveriesAvailableController();
+const updateEndDateController = new UpdateEndDateController();
 const updateDeliverymanController = new UpdateDeliveryManController();
 
 deliveriesRoutes.post("/", ensureAuthenticateClient, createDeliverController.handle);
 deliveriesRoutes.get("/available", ensureAuthenticateDeliveryman, findAllDeliveriesAvailableController.handle);
+deliveriesRoutes.put("/updateEndDate/:id", ensureAuthenticateDeliveryman, updateEndDateController.handle);
 deliveriesRoutes.put("/updateDeliveryman/:id", ensureAuthenticateDeliveryman, updateDeliverymanController.handle);
 
 export { deliveriesRoutes };

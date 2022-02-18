7595b053f47f0e1d1480068e221f6314f526824b
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-18 17:34:34 -0300

    test(authenticateAdminUseCase): added on Project.

---
+++ src/modules/account/useCases/authAdmin/authenticateAdminUseCase.spec.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../../../../database/prismaClient";
+import { adminApp } from "../../../../database/seed";
+import { AppError } from "../../../../shared/errors/appError";
+import { AuthenticateAdminUseCase } from "./authenticateAdminUseCase";
+
+const authenticateAdminUseCase = new AuthenticateAdminUseCase();
+
+describe("Autenticate Admin", () => {
+
+    it("Should be able to authenticate Admin on App", async () => {
+
+        let result = await authenticateAdminUseCase.execute({
+            username: adminApp.username,
+            password: String(adminApp.password)
+        })
+
+        expect(result).toHaveProperty('token');
+        expect(result).toHaveProperty('message');
+    });
+
+    it("Should not be able to authehticate with incorrect password", async () => {
+
+        await expect(authenticateAdminUseCase.execute({
+            username: adminApp.username,
+            password: "failed"
+        })).rejects.toEqual(new AppError("Username or password invalid!", 401))
+    });
+
+    it("Should not be able to authentica a non existent Admin", async () => {
+
+        await expect(authenticateAdminUseCase.execute({
+            username: "failedAdmin",
+            password: "failedPassword"
+        })).rejects.toEqual(new AppError("Username or password invalid!", 401));
+    })
+})

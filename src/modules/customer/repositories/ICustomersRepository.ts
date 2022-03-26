d6acf19b47105674d9031c110c98b7d1e1132448
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-26 19:38:32 -0300

    feat(src/modules/cusstomer/repositories/ICustomerRepository.ts): added two new functions: - findByEmail - findByUsername.

--- src/modules/customer/repositories/ICustomersRepository.ts
+++ src/modules/customer/repositories/ICustomersRepository.ts
@@ -4,6 +4,8 @@ import { ICreateCustomerDTO } from "../dtos/ICreateCustomerDTO"
 export interface ICustomersRepository {
     create({ email, username, password }: ICreateCustomerDTO): Promise<Customers>
     findAll(): Promise<Customers[]>
+    findByEmail(email: string): Promise<Customers>
     findById(id_customer: string): Promise<Customers>
+    findByUsername(username: string): Promise<Customers>
     findAllDeliveriesByCustomer(id_customer: string): Promise<Customers>
 }

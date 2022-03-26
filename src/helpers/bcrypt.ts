b15d8228077fae9563c97052a4cd3d456dc855dc
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-26 09:06:53 -0300

    feat(src/helpers): added "bcrypt.ts" for hash e decoded password. Create directory "helpers" on "src".

---
+++ src/helpers/bcrypt.ts
@@ -0,0 +1,16 @@
+import { AppError } from "../shared/errors/appError"
+import { hash, compare } from "bcrypt"
+
+export async function hashPassword(password: string) {
+    return await hash(password, 10)
+}
+
+export async function verifyPassword(password: string, comparePassword: string) {
+    const result = await compare(password, comparePassword)
+    
+    if (result == false) {
+        throw new AppError("User or Password Invalids", 401)
+    }
+
+    return result
+}

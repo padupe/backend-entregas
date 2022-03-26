9c945be42ccb5a0b4c438b4dd8850d5438c5cad5
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-26 19:25:33 -0300

    feat(src/modules/customer/infra/prisma/repositories): create class "CustomersRepository".
    Create directories "infra/prisma/repositories" on "src/modules/customer".

---
+++ src/modules/customer/infra/prisma/repositories/CustomersRepository.ts
@@ -0,0 +1,74 @@
+import { prisma } from "@database/prismaClient";
+import { ICreateCustomerDTO } from "@modules/customer/dtos/ICreateCustomerDTO";
+import { ICustomersRepository } from "@modules/customer/repositories/ICustomersRepository";
+import { Customers, PrismaClient } from "@prisma/client"
+
+class CustomersRepository implements ICustomersRepository {
+    private repository: PrismaClient
+
+    constructor() {
+        this.repository = prisma
+    }
+
+    async create({ email, username, password }: ICreateCustomerDTO): Promise<Customers> {
+        
+        const newCustomer = await prisma.customers.create({
+            data: {
+                email,
+                username,
+                password
+            }
+        })
+
+        return newCustomer
+    }
+    
+    async findAll(): Promise<Customers[]> {
+
+        const customers = await prisma.customers.findMany({
+            select: {
+                id: true,
+                email: true,
+                username: true
+            }
+        })
+        
+        //@ts-ignore
+        return customers
+    }
+    
+    async findById(id_customer: string): Promise<Customers> {
+        
+        const customer = await prisma.customers.findUnique({
+            where: {
+                id: id_customer
+            }
+        })
+
+        //@ts-ignore
+        return customer
+    }
+    
+    async findAllDeliveriesByCustomer(id_customer: string): Promise<Customers> {
+        
+        const deliveriesByCustomer = await prisma.customers.findUnique({
+            where: {
+                id: id_customer
+            },
+            select: {
+                id: true,
+                username: true,
+                deliveries: {
+                    select: {
+                        id: true,
+                        item_name: true,
+                        created_at: true,
+                    }
+                }
+            }           
+        })
+
+        //@ts-ignore
+        return deliveriesByCustomer
+    }
+}

378574dad69bc5d21872d8763465e8c301778377
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-27 11:33:59 -0300

    refactor(src/modules/customer/useCases/createConsumer/findAllConsumersController.ts): change to using tsyringe (containers).

--- src/modules/customer/useCases/findAllCustomers/findAllCustomersController.ts
+++ src/modules/customer/useCases/findAllCustomers/findAllCustomersController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 import { FindAllCustomersUseCase } from "./findAllCustomersUseCase";
 
 export class FindAllCustomersController {
 
     async handle(request: Request, response: Response){
 
-        const findAllCustomersUseCase = new FindAllCustomersUseCase();
+        const findAllCustomersUseCase = container.resolve(FindAllCustomersUseCase);
         const customers = await findAllCustomersUseCase.execute();
 
         return response.json(customers);

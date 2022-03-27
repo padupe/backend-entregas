892ff5764efe57eb172571c6de0679e0d049dbfa
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-27 00:03:35 -0300

    fix(src/shared/container/index.ts): change imports ordem.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,6 @@
+import { container } from "tsyringe";
 import { CustomersRepository } from "@modules/customer/infra/prisma/repositories/CustomersRepository"
 import { ICustomersRepository } from "@modules/customer/repositories/ICustomersRepository";
-import { container } from "tsyringe";
 
 container.registerSingleton<ICustomersRepository>(
     "CustomersRepository",

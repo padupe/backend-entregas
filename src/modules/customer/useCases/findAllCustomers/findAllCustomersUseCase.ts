b06f3926633f909a7c0988d975388be5648fae69
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-27 11:33:29 -0300

    refactor(src/modules/customer/useCases/createConsumer/findAllConsumersUseCase.ts): change to using tsyringe (containers).

--- src/modules/customer/useCases/findAllCustomers/findAllCustomersUseCase.ts
+++ src/modules/customer/useCases/findAllCustomers/findAllCustomersUseCase.ts
@@ -1,17 +1,19 @@
 import { prisma } from "@database/prismaClient";
+import { ICustomersRepository } from "@modules/customer/repositories/ICustomersRepository";
+import { inject, injectable } from "tsyringe";
 
+@injectable()
 export class FindAllCustomersUseCase {
+    
+    constructor(
+        @inject('CustomersRepository')
+        private customersRepository: ICustomersRepository
+    ){}
 
     async execute() {
 
-        const customers = await prisma.customers.findMany({
-            select: {
-                id: true,
-                email: true,
-                username: true
-            }
-        })
+        const customers = await this.customersRepository.findAll()
 
-        return customers;
+        return customers
     }
 }

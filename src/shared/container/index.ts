6a3704ba675423f19b0a97376b5216dfdbc5ff32
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-28 20:23:31 -0300

    feat(src/shared/container/index.ts): inserts on file: "AdminRepository".

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
 import { CustomersRepository } from "@modules/customer/infra/prisma/repositories/CustomersRepository"
 import { ICustomersRepository } from "@modules/customer/repositories/ICustomersRepository";
+import { IAdminRepository } from "@modules/account/repositories/IAdminRepository";
+import { AdminRepository } from "@modules/account/infra/prisma/repositories/AdminRepository";
 
 container.registerSingleton<ICustomersRepository>(
     "CustomersRepository",
     CustomersRepository
+)
+
+container.registerSingleton<IAdminRepository>(
+    "AdminRepository",
+    AdminRepository
 )

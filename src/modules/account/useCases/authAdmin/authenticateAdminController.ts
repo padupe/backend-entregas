37cbe389fbddd1acb3685b3884c648eb65843f36
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-28 20:28:27 -0300

    refactor(src/modules/account/useCases/authAdmin/authenticateAdminController.ts): using containers (tsyringe).
    After refactor on directory.

--- src/modules/account/useCases/authAdmin/authenticateAdminController.ts
+++ src/modules/account/useCases/authAdmin/authenticateAdminController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 import { AuthenticateAdminUseCase } from "./authenticateAdminUseCase";
 
 export class AuthenticateAdminController {
@@ -7,12 +8,8 @@ export class AuthenticateAdminController {
 
         const { username, password } = request.body;
 
-        const authenticateAdminUseCase = new AuthenticateAdminUseCase();
-        
-        const result = await authenticateAdminUseCase.execute({
-            username,
-            password
-        });
+        const authenticateAdminUseCase = container.resolve(AuthenticateAdminUseCase)
+        const result = await authenticateAdminUseCase.execute(request.body)
 
         return response.status(200).json(result)
     }
